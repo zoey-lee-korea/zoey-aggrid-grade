16592684fbb7cd876e1cf3efa13941723ac572e3
zoey <juhyelee@lgcns.com>
2024-04-17 14:53:22 +0900

    <LocalizationProvider dateAdapter={AdapterDayjs}> - App.js에 작성하여 전역 적용 : 앱의 모든 컴포넌트에서 사용가능하도록 전역적으로 적용 : 최상위 레벨에서 선언하면 React의 트리 구조에 따라 모든 하위 컴포넌트에 전파된다 : 하위의 컴포넌트에서 LocalizationProvider를 별도로 선언하면 해당 컴포넌트만의 커스텀이 가능하다
    - dateAdapter={AdapterDayjs}
    : Material-UI와 Day.js 사이의 어댑터 역할
    : Material-UI는 React를 위한 UI 라이브러리,
    Day.js는 날짜와 시간을 다루는 JavaScript 라이브러리로,
    각 라이브러리는 서로 다른 인터페이스를 가지고 있다 (용도 및 목적도 다르다)
    
    - 어댑터
    : 일반적으로 서로 다른 두 시스템이나 기술 사이의 인터페이스를 맞춰주는 역할을 한다
    : 한 시스템이 다른 시스템과 원활하게 작동하도록 도와주는 중간 매개체 기능
    ex) AdapterDayjs
    : Material-UI의 날짜와 시간 관련 컴포넌트들이 Day.js의 기능을 이용하여 날짜와 시간을 처리할 수 있도록
    중간에서 두 라이브러리의 인터페이스를 맞춰주는 역할

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 import Main from "./component/DashboardMain/Main";
 import SolutionDetail from "./component/SolutionDetail/SolutionDetail";
@@ -6,16 +8,18 @@ import SolutionDetail from "./component/SolutionDetail/SolutionDetail";
 const Home = () => {
     return (
         <div className="App">
-            <BrowserRouter basename="/zoey">
-                <Routes>
-                    {/* 메인 */}
-                    <Route path="/main_dashboard" element={<Main />}></Route>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <BrowserRouter basename="/zoey">
+                    <Routes>
+                        {/* 메인 */}
+                        <Route path="/main_dashboard" element={<Main />}></Route>
 
-                    {/* 전자/엔솔 개발진척 */}
-                    <Route path="/solution_detail" element={<SolutionDetail />}></Route>
+                        {/* 전자/엔솔 개발진척 */}
+                        <Route path="/solution_detail" element={<SolutionDetail />}></Route>
 
-                </Routes>
-            </BrowserRouter>
+                    </Routes>
+                </BrowserRouter>
+            </LocalizationProvider>
         </div>
     );
 };
