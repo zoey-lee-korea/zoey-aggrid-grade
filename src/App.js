0699145e53b4eefc25e4c9134c8645e5777ca945
zoey <juhyelee@lgcns.com>
2024-04-23 17:42:39 +0900

    [index.js는 컴포넌트의 진입점(entry point)가 된다]
    : Ex. Main이라는 폴더에 index.js를 만들면, <Main />으로 접근했을 때 index.js로 들어가진다
    
    - index.js
    : 주로 프로젝트의 진입점으로 사용
    : ReactDOM.render로 랜더링할 최상위 컴포넌트에 대한 코드를 작성
    
    - app.js
    : 주로 앱의 최상위 컴포넌트를 정의
    : 전체 애플리케이션 구조를 정의하는 역할

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
-import Main from "./component/DashboardMain/Main";
+import Main from "./component/Main";
 import Zoey from "./component/My/Zoey";
 import SolutionDetail from "./component/SolutionDetail/SolutionDetail";
 
@@ -10,13 +10,15 @@ const Home = () => {
     return (
         <div className="App">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <BrowserRouter basename="/zoey">
+                {/* <BrowserRouter basename="/zoey"> */}
+                <BrowserRouter>
                     <Routes>
                         {/* 개인 작업 */}
-                        <Route path="/" element={<Zoey />}></Route>
+                        <Route path="/zoey" element={<Zoey />}></Route>
 
                         {/* 메인 */}
-                        <Route path="/main_dashboard" element={<Main />}></Route>
+                        {/* <Route path="/main_dashboard" element={<Main />}></Route> */}
+                        <Route path="/main" element={<Main />}></Route>
 
                         {/* 전자/엔솔 개발진척 */}
                         <Route path="/solution_detail" element={<SolutionDetail />}></Route>
