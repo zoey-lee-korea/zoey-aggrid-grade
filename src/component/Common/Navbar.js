99bd5e6f07f6425d360edff9ca9ab9eba80a6a90
zoey <juhyelee@lgcns.com>
2024-04-24 14:55:34 +0900

    [ !! 논리연산자, 메인 icon 추가, Navbar.Link ]
    - !!
    : 값을 Boolean 형태로 변환하는데 사용
    : 첫번째 ! 는 값을 Boolean으로 변환하면서 결과를 반전시킨다
    Ex) true <-> false
    : 두번째 ! 는 반전된 값을 원래의 Boolean 값으로 돌려 준다
    : 즉 navList가 존재하면 true, 그렇지 않으면 false를 반환
    
    - image
    : <Image> 태그로 로고 이미지 설정
    
    - <Navbar.Collapse>
    - <Nav.Link>

--- src/component/Common/Navbar.js
+++ src/component/Common/Navbar.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import { Nav } from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
 
 import "../../assets/css/navbar.css";
+import favicon from "../../assets/images/favicon.png";
 
 export default function NavigationBar(props) {
-  const prjCode = props.prjCode;
-  const [prj_name, setPrjName] = useState("");
+  // OLD: 기존엔 props를 사용함. Ex) props.navList를 이용해 공통 코드에서 navList 목록을 받아온다
+  // const prjCode = props.prjCode;
+  // const [prj_name, setPrjName] = useState("");
+
+  const navList = ["메인", "상세", "zoey"]
 
   return (
     <Navbar
@@ -22,16 +27,55 @@ export default function NavigationBar(props) {
       }}
     >
       <Navbar.Brand
-        // href={`/erpdash/solution_detail/${prjCode}`} // ZOEY
+        // href={`/erpdash/solution_detail/${prjCode}`} // OLD
         href={`/main`}
         style={{ color: "#4D34B1", fontWeight: "700", marginLeft: "40px",padding:"12px 0" }}
       >
-        {/* {prj_name} // ZOEY */} 
-        zoey's study project
+        {/* {prj_name} // OLD */} 
+        <Image
+        src={favicon} // 이미지 경로
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="zoey"
+        />
       </Navbar.Brand>
+
       <div className="header-sub-title">
         ag-grid 연습
       </div>
+
+      <Navbar.Collapse
+        id="responsive-navbar-nav"
+        style={{ display: "flex", justifyContent: "flex-end" }}
+      >
+        {
+          // !!props.navList ? // OLD
+          !! navList ?
+            <Nav className="mr-auto">
+              {
+                navList.map((item, index) => {
+                  const NAVURL = {
+                    "메인": "main", 
+                    "상세": "detail", 
+                    "zoey": "zoey"
+                  };
+
+                  return(
+                    <Nav.Link
+                      key={index}
+                      href={`/${NAVURL[item]}`}
+                      className={`custom-tab`}
+                    >
+                      {item}
+                    </Nav.Link>
+                  )
+                })
+              }
+            </Nav>
+            : null
+        }
+      </Navbar.Collapse>
     </Navbar>
   );
 }
